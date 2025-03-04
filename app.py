5ce3e41054d43388bf132121679aeeaf284e38d8
Tammineni2001 <tammineni.appalanaidu@zensar.com>
2025-03-04 16:16:32 +0530

    updated code

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes.routes import main_blueprint
 
 app = Flask(__name__)
 
+
 app.register_blueprint(main_blueprint)
 
 @app.route('/')
@@ -11,7 +12,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
--- routes/routes.py
+++ routes/routes.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, request, jsonify
-from models import engine, session, Base, User, DatasetAccess
+from models import engine, session, Base
+from models import User, DatasetAccess
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import text
+
 
 main_blueprint = Blueprint('main', __name__)
 
@@ -9,14 +13,23 @@ def add_user():
     if not data or not 'USER_NAME' in data or not 'PASSWORD' in data or not 'ROLE_ID' in data:
         return jsonify({"message": "Invalid input"}), 400
 
-    new_user = User(
-        USER_NAME=data['USER_NAME'],
-        PASSWORD=data['PASSWORD'],
-        ROLE_ID=data['ROLE_ID']
-    )
-    session.add(new_user)
-    session.commit()
-    return jsonify({"message": "User added successfully"}), 201
+    try:
+        session.execute(
+            text("""
+            INSERT INTO "UTIL"."DATABBUDDY_USERS" (USER_NAME, PASSWORD, ROLE_ID)
+            VALUES (:user_name, :password, :role_id)
+            """),
+            {'user_name': data['USER_NAME'], 'password': data['PASSWORD'], 'role_id': data['ROLE_ID']}
+        )
+        session.commit()
+        return jsonify({"message": "User added successfully"}), 201
+    except IntegrityError:
+        session.rollback()
+        return jsonify({"message": "User already exists"}), 409
+    except Exception as e:
+        session.rollback()
+        return jsonify({"message": str(e)}), 500
+
 
 @main_blueprint.route('/add_dataset_access', methods=['POST'])
 def add_dataset_access():
@@ -24,29 +37,60 @@ def add_dataset_access():
     if not data or not 'ROLE_ID' in data or not 'ROLE_NAME' in data or not 'DATASET_ACCESS' in data:
         return jsonify({"message": "Invalid input"}), 400
 
-    new_access = DatasetAccess(
-        ROLE_ID=data['ROLE_ID'],
-        ROLE_NAME=data['ROLE_NAME'],
-        DATASET_ACCESS=data['DATASET_ACCESS']
-    )
-    session.add(new_access)
-    session.commit()
-    return jsonify({"message": "Dataset access added successfully"}), 201
+    try:
+        session.execute(
+            text("""
+            INSERT INTO "UTIL"."USER_ROLES" (ROLE_ID, ROLE_NAME, DATASET_ACCESS)
+            VALUES (:role_id, :role_name, :dataset_access)
+            """),
+            {'role_id': data['ROLE_ID'], 'role_name': data['ROLE_NAME'], 'dataset_access': data['DATASET_ACCESS']}
+        )
+        session.commit()
+        return jsonify({"message": "Dataset access added successfully"}), 201
+    except IntegrityError:
+        session.rollback()
+        return jsonify({"message": "Role already exists"}), 409
+    except Exception as e:
+        session.rollback()
+        return jsonify({"message": str(e)}), 500
+
 
 @main_blueprint.route('/user_roles', methods=['GET'])
 def get_user_roles():
-    users = session.query(User).all()
-    user_list = [{"USER_NAME": user.USER_NAME, "ROLE_ID": user.ROLE_ID} for user in users]
-    return jsonify(user_list), 200
-
+    try:
+        
+        result = session.execute(text('SELECT * FROM "UTIL"."USER_ROLES"'))
+        users = result.fetchall()
+        user_list = [{"USER_NAME": row[0], "ROLE_ID": row[1]} for row in users]
+        session.commit()  
+        return jsonify(user_list), 200
+    except Exception as e:
+        session.rollback() 
+        return jsonify({"message": str(e)}), 500
+        
+       
+        
 @main_blueprint.route('/update_role/<string:username>', methods=['PUT'])
 def update_role(username):
     data = request.get_json()
     if not data or not 'ROLE_ID' in data:
         return jsonify({"message": "Invalid input"}), 400
-    user = session.query(User).filter(User.USER_NAME == username).first()
-    if user:
-        user.ROLE_ID = data["ROLE_ID"]
-        session.commit()
-        return jsonify({"message": "Role updated successfully"}), 200
-    return jsonify({"message": "User not found"}), 404
+
+    try:
+        result = session.execute(
+            text("""
+            UPDATE "UTIL"."DATABBUDDY_USERS"
+            SET ROLE_ID = :role_id
+            WHERE USER_NAME = :username
+            """),
+            {'role_id': data['ROLE_ID'], 'username': username}
+        )
+        
+        if result.rowcount > 0:
+            session.commit()
+            return jsonify({"message": "Role updated successfully"}), 200
+        else:
+            return jsonify({"message": "User not found"}), 404
+    except Exception as e:
+        session.rollback()
+        return jsonify({"message": str(e)}), 500
