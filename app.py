62d8c4037af7041ad70be5e4b7a3323faca6af3b
Tammineni2001 <tammineni.appalanaidu@zensar.com>
2025-03-11 14:58:23 +0530

    latest code

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, jsonify
 from routes.routes import main_blueprint
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app)
+
+# Swagger configuration
+SWAGGER_URL = '/api-documentation'
+API_URL = '/static/swagger.yaml' 
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Task Application API" 
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 
 app.register_blueprint(main_blueprint)
